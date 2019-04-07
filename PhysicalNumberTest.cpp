14885eaeaf35905c718de84dc370034ab1eb557d
ChenOst <44844647+ChenOst@users.noreply.github.com>
2019-04-07 21:03:08 +0300

    Add files via upload

--- PhysicalNumberTest.cpp
+++ PhysicalNumberTest.cpp
@@ -24,6 +24,16 @@ int main() {
     PhysicalNumber c(2, Unit::HOUR);
     PhysicalNumber d(30, Unit::MIN);
 
+    PhysicalNumber pn1(25, Unit::CM);
+    PhysicalNumber pn2(5, Unit::M);
+    PhysicalNumber pn3(13, Unit::KM);
+    PhysicalNumber pn4(7, Unit::SEC);
+    PhysicalNumber pn5(9, Unit::MIN);
+    PhysicalNumber pn6(225, Unit::HOUR);
+    PhysicalNumber pn7(2, Unit::G);
+    PhysicalNumber pn8(140, Unit::KG);
+    PhysicalNumber pn9(84, Unit::TON);
+
     testcase
     .setname("Basic output")
     .CHECK_OUTPUT(a, "2[km]")
@@ -50,6 +60,40 @@ int main() {
     .CHECK_OUTPUT((a += PhysicalNumber(1, Unit::TON)), "1700[kg]")
 
     // YOUR TESTS - INSERT AS MANY AS YOU WANT
+    .setname("My test - Basic output")
+
+    .CHECK_OUTPUT(pn1, "25[cm]")
+    .CHECK_OUTPUT(pn2, "5[m]")
+    .CHECK_OUTPUT(pn3, "13[km]")
+    .CHECK_OUTPUT(pn4, "7[sec]")
+    .CHECK_OUTPUT(pn5, "9[min]")
+    .CHECK_OUTPUT(pn6, "225[hour]")
+    .CHECK_OUTPUT(pn7, "2[g]")
+    .CHECK_OUTPUT(pn8, "140[kg]")
+    .CHECK_OUTPUT(pn9, "84[ton]")
+
+    .setname("My test - Compatible dimensions")
+    .CHECK_OUTPUT(pn2+pn1, "5.25[m]")
+    .CHECK_OUTPUT(pn3+pn2, "13.005[km]")
+    .CHECK_OUTPUT(pn3, "13.005[km]")
+    .CHECK_OUTPUT(pn6-pn6, "0[hour]")
+    .CHECK_OUTPUT(pn7+pn8, "140002[g]")
+
+    .setname("My test - Incompatible dimensions")
+    .CHECK_THROWS(pn1+pn6)
+    .CHECK_THROWS(pn2-pn9)
+    .CHECK_THROWS(pn3+pn7)
+    .CHECK_THROWS(pn4-pn1)
+    .CHECK_THROWS(pn5+pn8)
+    .CHECK_THROWS(pn6-pn3)
+    .CHECK_THROWS(pn7+pn2)
+    .CHECK_THROWS(pn8-pn5)
+    .CHECK_THROWS(pn9+pn4)
+    .CHECK_OK(istringstream("700[kg]") >> pn1)
+    .CHECK_OK(istringstream("700[kg]") >> pn3)
+    .CHECK_OK(istringstream("700[kg]") >> pn5)
+    .CHECK_OK(istringstream("700[kg]") >> pn7)
+    .CHECK_OK(istringstream("700[kg]") >> pn9)
 
       .setname("...")
 
